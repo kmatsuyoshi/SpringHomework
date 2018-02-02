38372e5cf2273069e81814ad29fa7bf2a573ecb3
kmatsuyoshi <kmatsuyoshi@stuy.edu>
2018-02-01 22:18:45 -0500

    fixed hw03 once again

--- hw03/MatrixFinder.java
+++ hw03/MatrixFinder.java
@@ -1,78 +1,42 @@
-// Team Cookie (Kevin Lin, Victor Lin, Kayli Matsuyoshi)
-// APCS2 pd1
-// HW03 -- Searching in a 2D Array
-// 2018-02-02f
+//Team Cookie - Kayli Matsuyoshi, Kevin Lin, Victor Lin
+//APCS2 -- Pd1
+//HW03 -- I Am Still Searching
+//2018-02-02
 
 /* SUMMARY
-   In our method we decided to compare the target value to the value at the indices, starting with the value at the upper right-hand corner, array[0][n-1]. We are starting from this value because it is the first pivotal point of comparison, meaning that it is the largest value in its own row, but the smallest value in its own row. Anything to the left of it will be less than it, and anything greater than it will be below it. 
+   In our method we decided to compare the target value to the value at the indices, starting with the value at the upper right-hand corner, array[0][n-1]. We are starting from this value because it is the first pivotal point of comparison, meaning that it is the largest value in its own row, but the smallest value in its own row. Anything to the left of it will be less than it, and anything greater than it will be below it.
    By using a while loop, we are able to make repetitive comparisons depending on if the value at the index is equal to the target. The while loop returns (-1,-1) if no matches are made, and returns the coordinates once the target is found.
 */
 
 public class MatrixFinder {
-    
-    public static String searcher( int[][] array, int target ) {
 
-	String returnString = "";
-	int n = array[0].length;
-	int row = 0;
-	int col = n - 1;
-
-	if ( target < array[0][0] || target > array[n-1][n-1] ) {
-
-	    returnString = "(-1, -1)";
-
-	}
-
-	else {
-
-	    while ( row < n && row >= 0 && col < n && col >= 0 ) {
-
-		if ( target == array[row][col] ) {
-
-		    returnString = "(" + row + ", " + col + ")";
-		    break;
-
-		}
-
-		else {
-
-		    if ( target > array[row][col] ) {
-
-			row += 1;
-			
-		    }
-
-		    else {
-
-			col -= 1;
-
-		    }
-
-		}
-
-	    }
-	    
-	}
-
-	return returnString;
-
-    } // end of searcher
-
-    public static void main( String[] args ) {
-
-	int n = 4;
-	
-        int[][] _matrix = new int[n][n];
-	int i = 1;
-	for ( int x = 0; x < n; x++ ) {
-	    for ( int y = 0; y < n; y++ ) {
-		_matrix[x][y] = i * 2;
-		i += 1;
-	    }
-	}
-
-	System.out.println( searcher( _matrix, 18 ));
-
+  public static String find(int[][] matrix, int target) {
+    int column = matrix[0].length-1;
+    int row = 0;
+    while (row < matrix.length && column >= 0) {
+      if (matrix[row][column] == target) {return "(" + row + "," + column + ")";}
+      if (matrix[row][column] > target) {column -= 1;}
+      else {row += 1;}
     }
+    return "(-1,-1)";
+    
+  }
 
-} // end of class MatrixFinder
+  public static void main(String[] args) {
+
+    int[][] matrix = { {1,2,3,4},
+                       {2,3,4,5},
+                       {3,4,5,6},
+                       {4,5,6,7} };
+
+    int[][] matrix2 = { { 2, 4, 6, 8},
+                        {10,12,14,16},
+                        {18,20,22,24},
+                        {26,28,30,32} };
+
+    System.out.println(MatrixFinder.find(matrix,5)); //(1,3)
+    System.out.println(MatrixFinder.find(matrix2,3)); //(-1,-1)
+
+  }
+
+}
