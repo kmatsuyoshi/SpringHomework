00548ece7305dec697be7e43caf2a44613712fd6
kmatsuyoshi <kmatsuyoshi@stuy.edu>
2018-04-30 16:11:19 -0400

    ohno

--- hw42/BST.java
+++ hw42/BST.java
@@ -14,106 +14,107 @@
 public class BST
 {
 
-  //instance variables / attributes of a BST:
-  TreeNode _root;
+    // instance variables / attributes of a BST:
+    TreeNode _root;
 
-  /*****************************************************
-   * default constructor
-   *****************************************************/
-  BST( )
-  {
-    /*** YOUR IMPLEMENTATION HERE ***/
-  }
+    /*****************************************************
+     * default constructor
+     *****************************************************/
+    BST( )
+    {
+	_root = null;
+    }
+    /*****************************************************
+     * void insert( int )
+     * Adds a new data element to tree.
+     *****************************************************/
+    public void insert( int newVal )
+    {
+	TreeNode newNode = new TreeNode( newVal );
+	if ( _root = null ) {
+	    _root = newNode;
+	    return;
+	}
+	insert( _root, newNode );
+    }
+    //recursive helper for insert(int)
+    public void insert( TreeNode stRoot, TreeNode newNode )
+    {
+        if ( stRoot.getLeft() < newNode ) {
+	    
+	}
+    }//end insert()
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //~~~~~~~~~~~~~v~~TRAVERSALS~~v~~~~~~~~~~~~~~~~~~~~~
+
+    // each traversal should simply print to standard out
+    // the nodes visited, in order
+
+    //process root, recurse left, recurse right
+    public void preOrderTrav()
+    {
+	preOrderTrav( _root );
+    }
+    public void preOrderTrav( TreeNode currNode )
+    {
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+    //recurse left, process root, recurse right
+    public void inOrderTrav()
+    {
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+    public void inOrderTrav( TreeNode currNode )
+    {
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+    // recurse left, recurse right, process root
+    public void postOrderTrav()
+    {
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+    public void postOrderTrav( TreeNode currNode )
+    {
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+    //~~~~~~~~~~~~~^~~TRAVERSALS~~^~~~~~~~~~~~~~~~~~~~~~
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-  /*****************************************************
-   * void insert( int )
-   * Adds a new data element to tree.
-   *****************************************************/
-  public void insert( int newVal )
-  {
-    TreeNode newNode = new TreeNode( newVal );
-    /*** YOUR IMPLEMENTATION HERE ***/
-  }
-  //recursive helper for insert(int)
-  public void insert( TreeNode stRoot, TreeNode newNode )
-  {
-    /*** YOUR IMPLEMENTATION HERE ***/
-  }//end insert()
+    //main method for testing
+    public static void main( String[] args )
+    {
+	/*~~~~~~~~~~~~move~me~down~~~~~~~~~~~~~~~~~~~~~~
 
+	  BST arbol = new BST();
 
+	  //PROTIP: sketch state of tree after each insertion
+	  //        ...BEFORE executing these.
+	  arbol.insert( 4 );
+	  arbol.insert( 2 );
+	  arbol.insert( 5 );
+	  arbol.insert( 6 );
+	  arbol.insert( 1 );
+	  arbol.insert( 3 );
 
+	  System.out.println( "\n-----------------------------");
+	  System.out.println( "pre-order traversal:" );
+	  arbol.preOrderTrav();
 
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  //~~~~~~~~~~~~~v~~TRAVERSALS~~v~~~~~~~~~~~~~~~~~~~~~
+	  System.out.println( "\n-----------------------------");
+	  System.out.println( "in-order traversal:" );
+	  arbol.inOrderTrav();
 
-  // each traversal should simply print to standard out
-  // the nodes visited, in order
+	  System.out.println( "\n-----------------------------");
+	  System.out.println( "post-order traversal:" );
+	  arbol.postOrderTrav();
 
-  //process root, recurse left, recurse right
-  public void preOrderTrav()
-  {
-    preOrderTrav( _root );
-  }
-  public void preOrderTrav( TreeNode currNode )
-  {
-    /*** YOUR IMPLEMENTATION HERE ***/
-  }
-
-  //recurse left, process root, recurse right
-  public void inOrderTrav()
-  {
-    /*** YOUR IMPLEMENTATION HERE ***/
-  }
-  public void inOrderTrav( TreeNode currNode )
-  {
-    /*** YOUR IMPLEMENTATION HERE ***/
-  }
-
-  //recurse left, recurse right, process root
-  public void postOrderTrav()
-  {
-    /*** YOUR IMPLEMENTATION HERE ***/
-  }
-  public void postOrderTrav( TreeNode currNode )
-  {
-    /*** YOUR IMPLEMENTATION HERE ***/
-  }
-
-  //~~~~~~~~~~~~~^~~TRAVERSALS~~^~~~~~~~~~~~~~~~~~~~~~
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-  //main method for testing
-  public static void main( String[] args )
-  {
-    /*~~~~~~~~~~~~move~me~down~~~~~~~~~~~~~~~~~~~~~~
-
-      BST arbol = new BST();
-
-      //PROTIP: sketch state of tree after each insertion
-      //        ...BEFORE executing these.
-      arbol.insert( 4 );
-      arbol.insert( 2 );
-      arbol.insert( 5 );
-      arbol.insert( 6 );
-      arbol.insert( 1 );
-      arbol.insert( 3 );
-
-      System.out.println( "\n-----------------------------");
-      System.out.println( "pre-order traversal:" );
-      arbol.preOrderTrav();
-
-      System.out.println( "\n-----------------------------");
-      System.out.println( "in-order traversal:" );
-      arbol.inOrderTrav();
-
-      System.out.println( "\n-----------------------------");
-      System.out.println( "post-order traversal:" );
-      arbol.postOrderTrav();
-
-      System.out.println( "\n-----------------------------");
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  }
+	  System.out.println( "\n-----------------------------");
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    }
 
 }//end class
